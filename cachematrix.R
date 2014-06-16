0d6509374ad5155b1a4836016f3b397093800186
rockmt <rockmt.yu@gmail.com>
2014-06-17 01:10:52 +0900

    commited cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+## [example]
+##  > mdata <- makeCacheMatrix(matrix(2:5,2,2))
+##  > cacheSolve(mdata)
+##  > cacheSolve(mdata)  // useing cache data
 
-## Write a short comment describing this function
-
+## makeCacheMatrix : return a list "matrix data"
+## 1. set the matrix values
+## 2. get the matrix values
+## 3. set the inverse matrix values
+## 4. get the inverse matrix values
 makeCacheMatrix <- function(x = matrix()) {
+
+  v <- NULL;
+
+  # setter of matrix values
+  set <- function(y) {
+
+    x <<- y;
+
+    v <<- NULL;
+
+  }
+
+  # getter of matrix values
+  get <- function() {
+    x;
+  }
+
+  # setter of inverse matrix values
+  setInv <- function(solve) {
+    v <<- solve;
+  }
+
+  # getter of inverse matrix values
+  getInv <- function() {
+    v;
+  }
+
+  # output data matrix
+  list(set=set, get=get, setInv=setInv, getInv=getInv);
 
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve : get the inverse matrix data.
+## if the inverse matrix data is already stored, return from cache data.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  ## Return a matrix that is the inverse of 'x'
+
+  # get inverse matrix values
+  v <- x$getInv()
+
+  # if values not null, return values
+  if (!is.null(v)) {
+    message("using cached data values");
+    return(v);
+  }
+
+  data <- x$get();
+
+  v <- solve(data, ...);
+
+  # cache inverse matrix values
+  x$setInv(v);
+
+  # return values
+  v;
+
 }
